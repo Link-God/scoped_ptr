98909160596e733d04f7c5f5168d88486bbcc16f
Yury <32190920+Link-God@users.noreply.github.com>
2018-04-20 23:50:20 +0300

    Update scoped_ptr.hpp

--- include/scoped_ptr.hpp
+++ include/scoped_ptr.hpp
@@ -20,3 +20,46 @@ struct X
 {
 	int m ;
 };
+
+template<typename T>
+	scoped_ptr<T>::scoped_ptr(T * ptr )
+	{
+		ptr_ = ptr;
+	}
+
+	template<typename T>
+	scoped_ptr<T>::~scoped_ptr()
+	{
+		delete ptr_;
+	}
+
+	template<typename T>
+	void scoped_ptr<T>::reset(T * ptr )
+	{
+		if (ptr_) delete ptr_;
+		ptr_ = ptr;
+	}
+
+	template<typename T>
+	T & scoped_ptr<T>:: operator*() const
+	{
+		if (ptr_) return *ptr_;
+	}
+
+	template<typename T>
+	T * scoped_ptr<T>::operator->() const
+	{
+		if (ptr_) return ptr_;
+	}
+
+	template<typename T>
+	T * scoped_ptr<T>::get() const
+	{
+		return ptr_;
+	}
+
+	template<typename T>
+	void scoped_ptr<T>::swap(scoped_ptr & other)
+	{
+		std::swap(ptr_, other.ptr_);
+	}
