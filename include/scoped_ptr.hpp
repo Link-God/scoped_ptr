0fba9dc932d10f24d1d8c57edd80278f2e24c42c
Yury <32190920+Link-God@users.noreply.github.com>
2018-04-20 14:47:51 +0300

    Create scoped_ptr.hpp

---
+++ include/scoped_ptr.hpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+template<class T>
+class scoped_ptr {
+private:
+	T * ptr_;
+public:
+	explicit scoped_ptr(T * ptr = 0);
+	~scoped_ptr();
+	void reset(T * ptr = 0);
+	T & operator*() const;
+	T * operator->() const;
+	T * get() const;
+	void swap(scoped_ptr & other);
+	scoped_ptr & operator=(scoped_ptr const &) = delete;
+	scoped_ptr(scoped_ptr const &) = delete;
+};
