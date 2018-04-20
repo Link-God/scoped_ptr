a3584f6433bfc25a8dfe0b781b334ee7fcf165b2
Yury <32190920+Link-God@users.noreply.github.com>
2018-04-20 21:34:48 +0300

    Update scoped_ptr.cpp

--- source/scoped_ptr.cpp
+++ source/scoped_ptr.cpp
@@ -1,45 +1,44 @@
 
 #include scoped_ptr.hpp
+	template<typename T>
+	scoped_ptr<T>::scoped_ptr(T * ptr )
+	{
+		ptr_ = ptr;
+	}
 
-template<typename T>
-scoped_ptr::scoped_ptr(T * ptr = 0)
-{
-	ptr_ = ptr;
-}
+	template<typename T>
+	scoped_ptr<T>::~scoped_ptr()
+	{
+		delete ptr_;
+	}
 
-template<typename T>
-scoped_ptr::~scoped_ptr()
-{
-	delete ptr_;
-}
+	template<typename T>
+	void scoped_ptr<T>::reset(T * ptr )
+	{
+		if (ptr_) delete ptr_;
+		ptr_ = ptr;
+	}
 
-template<typename T>
-scoped_ptr::reset(T * ptr = 0)
-{
-	if (ptr_) delete ptr_;
-	ptr_ = ptr;
-}
+	template<typename T>
+	T & scoped_ptr<T>:: operator*() const
+	{
+		if (ptr_) return *ptr_;
+	}
 
-template<typename T>
-scoped_ptr:: operator*() const
-{
-	if (ptr_) return *ptr_;
-}
+	template<typename T>
+	T * scoped_ptr<T>::operator->() const
+	{
+		if (ptr_) return ptr_;
+	}
 
-template<typename T>
-scoped_ptr::operator->() const
-{
-	if (ptr_) return ptr_;
-}
+	template<typename T>
+	T * scoped_ptr<T>::get() const
+	{
+		return ptr_;
+	}
 
-template<typename T>
-scoped_ptr::get() const
-{
-	return ptr_;
-}
-
-template<typename T>
-scoped_ptr::swap(scoped_ptr & other)
-{
-	std::swap(ptr_, other.ptr_);
-}
+	template<typename T>
+	void scoped_ptr<T>::swap(scoped_ptr & other)
+	{
+		std::swap(ptr_, other.ptr_);
+	}
